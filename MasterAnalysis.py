6d1657e5e47dc3a125c62a2a795bbe904fc3f6df
smileyfaceofdoom <u0776153@utah.edu>
2015-07-07 09:30:16 -0600

    change to Runge Kutta method

--- MasterAnalysis.py
+++ MasterAnalysis.py
@@ -25,6 +25,7 @@ import MyRootFinding
 import binning
 import ani
 import dfunc
+import RK4Dyn
 
 
 
@@ -40,13 +41,13 @@ start_time = time.clock()
 dyn = True # sets whether the simulation is dynamic (True) or quasistatic (False).
 damage = True #sets whether or not to add damage effects
 bing = False #sets whether or not to use binning
-testing = True #sets whether to run in testing mode, with a non-random initial number distribution
+testing = False #sets whether to run in testing mode, with a non-random initial number distribution
 auto_dfix = True #If true, if a displacement value is >= H, it will automatically be set to .9999*H. If false, the code won't run if a d value >= H.
-multirun = False #sets whether the simulation will run multiple times to make a comparison plot (if true, won't plot average or run animation)
+multirun = True #sets whether the simulation will run multiple times to make a comparison plot (if true, won't plot average or run animation)
 
 
 #set animation options
-make_ani = True
+make_ani = False
 save_ani = False
 auto_frameskip = True #automatically sets frameskip so that if save_ani is on, the animation will play in the same length of time but at 10fps
 
@@ -56,22 +57,22 @@ auto_frameskip = True #automatically sets frameskip so that if save_ani is on, t
 #basics
 ea_t = 10.0 #top spring constant (N)
 k_t = 100.0 #side spring constant (N/m)
-H = 2.0 #total height (m)
+H = 1.0 #total height (m)
 Wm = 4.0 #Weibull modulus
-n_l = 4 #number of links
+n_l = 10 #number of links
 steps = 1000 #number of displacement steps (note, if dyn=True this will be overwritten by t_stop/dt)
 
 #for binning
 i_max = 21 #number of bins
 
 #for damage
-theta_crit = 30.0 #breaking angle, measured from vertical (degrees)
+theta_crit = 20.0 #breaking angle, measured from vertical (degrees)
 
 #for dynamics
 m = 1.0 #mass (kg)
 g = 0.0 #acceleration of gravity (m/s^2)
-dt = .02 #time step (s)
-t_stop = 20.0 #total time (s)
+dt = .01 #time step (s)
+t_stop = 10.0 #total time (s)
 #if the animation is dynamic, set the number of steps based on the stop time and time step
 if dyn:
     steps = int(t_stop/dt)
@@ -95,10 +96,10 @@ runs = 100 #number of runs for multirun
 
 #create list of displacements using dfunc
 d = dfunc.cospath(steps,3,H,.5)
-# d1 = dfunc.linepath(steps/3,H*.5)
-# d2 = dfunc.pausepath(steps/3,H*.5)
+#d1 = dfunc.linepath(steps/4,H*.4)
+#d2 = dfunc.pausepath(steps/2,H*.4)
 # d3 = dfunc.linepath(steps/3,H,d0=H*.5)
-# d=d1+d2+d3
+#d=d1+d2
 
 
 
@@ -206,6 +207,7 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
         #for testing purposes
         R = [0.1406236293192208, 0.557452455836349, 0.4018884612118805, 0.8610494090625574, 0.005928894753714831]
         #R = linspace(.3*H,.8*H,n_l)
+        #R = [.5]
     else:
         #generate list of random numbers
         R = [random() for x in range(n_l)]
@@ -285,9 +287,6 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
         #initialize velocity
         v = [0]*n
         v_avg = 0
-        #initialize acceleration
-        a = 0
-        a_avg = 0
     
     
     #calculate force at each displacement value
@@ -376,79 +375,30 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
                     
                 #for dynamic
                 else:
-                    #solve ODE for y with predictor-corrector method
+                    #solve ODE for y with 4th order Runge Kutta method
                     
-                    #calculate the acceleration
                     #if the link isn't broken
                     if b[j] == 0:
-                        #if the argument for atanh steps over the lower boundary, set to lowest possible value to correct
-                        if (d[i-1]-y[i-1][j])/(H-L[j]) <= -1:
-                            print "invalid atanh argument"
-                            a = (k/(4*m))*(L[j]-y[i-1][j]) - (ea/m)*math.atanh(-.9999999999999999) - g
-                        #if the argument for atanh steps over the upper boundary, set to highest possible value to correct
-                        elif (d[i-1]-y[i-1][j])/(H-L[j]) >= 1:
-                            print "invalid atanh argument"
-                            a = (k/(4*m))*(L[j]-y[i-1][j]) - (ea/m)*math.atanh(.9999999999999999) - g
-                        #otherwise, calculate as normal
-                        else:
-                            a = (k/(4*m))*(L[j]-y[i-1][j]) - (ea/m)*math.atanh((d[i-1]-y[i-1][j])/(H-L[j])) - g
-                    #if the link is broken, the side spring and thus the link provides no force
+                        #run Runge Kutta method to find y and the velocity
+                        y[i][j],v1 = RK4Dyn.RK4(dt,d[i-1],d[i],y[i-1][j],v[j],H,L[j],k,ea,m,g)
+                        #store new velocity value
+                        v[j] = v1
                     else:
-                        #if the argument for atanh steps over the lower boundary, set to lowest possible value to correct
-                        if (d[i-1]-y[i-1][j])/(H-L[j]) <= -1:
-                            print "invalid atanh argument"
-                            a = -(ea/m)*math.atanh(-.9999999999999999) - g
-                        #if the argument for atanh steps over the upper boundary, set to highest possible value to correct
-                        elif (d[i-1]-y[i-1][j])/(H-L[j]) >= 1:
-                            print "invalid atanh argument"
-                            a = -(ea/m)*math.atanh(.9999999999999999) - g
-                        #otherwise, calculate as normal
-                        else:
-                            a = -(ea/m)*math.atanh((d[i-1]-y[i-1][j])/(H-L[j])) - g
-                     
-                    #calculate intermediate v
-                    v1 = v[j] + a*dt
+                        #run Runge Kutta method to find y and the velocity, with k=0 for breakage
+                        y[i][j],v1 = RK4Dyn.RK4(dt,d[i-1],d[i],y[i-1][j],v[j],H,L[j],0,ea,m,g)
+                        #store new velocity value
+                        v[j] = v1
                     
-                    #calculate intermediate y
-                    y1 = y[i-1][j] - v[j]*dt
-                    
-                    #if y1 has stepped over the boundary for the allowable atanh argument, correct
-                    #if y1 is too negative, increase slightly
-                    if d[i]-y1 >= H-L[j]:
-                        y1 = d[i]-H+L[j]+.001*H
-                    #if y1 is too positive, decrease slightly
-                    elif d[i]-y1 <= L[j]-H:
-                        y1 = d[i]+H-L[j]-.001*H
-                    
-                    #calculate intermediate acceleration
-                    #if the link isn't broken
-                    if b[j] == 0:
-                        a1 = (k/(4*m))*(L[j]-y1) - (ea/m)*math.atanh((d[i]-y1)/(H-L[j])) - g
-                    #if the link is broken (no link force)
-                    else:
-                        a1 = -(ea/m)*math.atanh((d[i]-y1)/(H-L[j])) - g
-                    
-                    #apply predictor-corrector formula to find new v
-                    v[j] = v[j] + (dt/2)*(a1+a)
-                    
-                    #apply predictor-corrector formula to find new y
-                    y[i][j] = y[i-1][j] - (dt/2)*(v1+v[j])
-                    
-                    #if y[i][j] has stepped over the boundary for the allowable atanh argument, correct and calculate force
-                    #if y[i][j] is too negative, increase slightly
+                    #correct y value if necessary
+                    #make sure the displacement doesn't pass the top of the link
                     if d[i]-y[i][j] >= H-L[j]:
-                        y_corr = d[i]-H+L[j]+.001*H
-                        #calculate force using the corrected y value
-                        F[j] = ea*math.atanh((d[i]-y_corr)/(H-L[j]))
-                    #if y[i][j] is too positive, decrease slightly
-                    elif d[i]-y[i][j] <= L[j]-H:
-                        y_corr = d[i]+H-L[j]-.001*H
-                        #calculate force using the corrected y value
-                        F[j] = ea*math.atanh((d[i]-y_corr)/(H-L[j]))
-                    #if y[i][j] is fine, calculate normally
-                    else:
-                        #calculate force using the y value
-                        F[j] = ea*math.atanh((d[i]-y[i][j])/(H-L[j]))
+                        y[i][j] = d[i] - (H-L[j])*.999999
+                    #make sure the spring doesn't stretch too long
+                    if d[i]-y[i][j] <= L[j]-H:
+                        y[i][j] = d[i] - (L[j]-H)*.999999
+                    
+                    #calculate force
+                    F[j] = ea*math.atanh((d[i]-y[i][j])/(H-L[j]))
                 
                 #if damage is turned on and the link hasn't broken, check to see if it has
                 if b[j] == 0 and damage:
@@ -467,6 +417,7 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
                 if y[i][j] >= L[j]:
                     #set y correctly
                     y[i][j] = L[j]
+                    
                     #set indicator to collapsed
                     c[j] = 2
                     #recalculate force to reflect collapse
@@ -496,9 +447,15 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
                 #print 'c', 2 
                 #store y
                 y[i][j] = L[j]
-                
+
                 #calculate the force in the link
-                F[j] = ea*math.atanh((d[i] - L[j])/(H - L[j]))
+                #if the atanh argument <= -1
+                if (d[i] - L[j])/(H - L[j]) <= -1:
+                    #make the force as small as possible
+                    F[j] = ea*math.atanh(-.9999999999999999)
+                #otherwise calculate normally
+                else:
+                    F[j] = ea*math.atanh((d[i] - L[j])/(H - L[j]))
                 
                 #check to see if the link is uncollapsing
                 #for dynamics, check if the spring force will overcome gravity
@@ -581,72 +538,28 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
                 
             #for dynamic
             else:
-                #solve ODE for y with predictor-corrector method
-                
-                #calculate the acceleration
+                #solve ODE for y with 4th order Runge Kutta method
+                    
                 #if the link isn't broken
-                if b_avg == 0:
-                    #if the argument for atanh steps over the lower boundary, set to lowest possible value to correct
-                    if (d[i-1]-y_avg[i-1])/(H-L_avg) <= -1:
-                        print "invalid atanh argument"
-                        a_avg = (k/(4*m))*(L_avg-y_avg[i-1]) - (ea/m)*math.atanh(-.9999999999999999) - g
-                    #if the argument for atanh steps over the upper boundary, set to highest possible value to correct
-                    elif (d[i-1]-y_avg[i-1])/(H-L_avg) >= 1:
-                        print "invalid atanh argument"
-                        a_avg = (k/(4*m))*(L_avg-y_avg[i-1]) - (ea/m)*math.atanh(.9999999999999999) - g
-                    #otherwise, calculate as normal
-                    else:
-                        a_avg = (k/(4*m))*(L_avg-y_avg[i-1]) - (ea/m)*math.atanh((d[i-1]-y_avg[i-1])/(H-L_avg)) - g
-                #if the link is broken, the side spring and thus the link provides no force
+                if b[j] == 0:
+                    #run Runge Kutta method to find y and the velocity
+                    y_avg[i],v1_avg = RK4Dyn.RK4(dt,d[i-1],d[i],y_avg[i-1],v_avg,H,L_avg,k,ea,m,g)
+                    #store new velocity value
+                    v_avg = v1_avg
                 else:
-                    #if the argument for atanh steps over the lower boundary, set to lowest possible value to correct
-                    if (d[i-1]-y_avg[i-1])/(H-L_avg) <= -1:
-                        print "invalid atanh argument"
-                        a_avg = -(ea/m)*math.atanh(-.9999999999999999) - g
-                    #if the argument for atanh steps over the upper boundary, set to highest possible value to correct
-                    elif (d[i-1]-y_avg[i-1])/(H-L_avg) >= 1:
-                        print "invalid atanh argument"
-                        a_avg = -(ea/m)*math.atanh(.9999999999999999) - g
-                    #otherwise, calculate as normal
-                    else:
-                        a_avg = -(ea/m)*math.atanh((d[i-1]-y_avg[i-1])/(H-L_avg)) - g
-                 
-                #calculate intermediate v_avg
-                v1_avg = v_avg + a_avg*dt
+                    #run Runge Kutta method to find y and the velocity, with k=0 for breakage
+                    y_avg[i],v1_avg = RK4Dyn.RK4(dt,d[i-1],d[i],y_avg[i-1],v_avg,H,L_avg,0,ea,m,g)
+                    #store new velocity value
+                    v_avg = v1
                 
-                #calculate intermediate y_avg
-                y1_avg = y_avg[i-1] - v_avg*dt
-                
-                #if y1_avg has stepped over the boundary for the allowable atanh argument, correct
-                #if y1_avg is too negative, increase slightly
-                if d[i]-y1_avg >= H-L_avg:
-                    y1_avg = d[i]-H+L_avg+.001*H
-                #if y1_avg is too positive, decrease slightly
-                elif d[i]-y1_avg <= L_avg-H:
-                    y1_avg = d[i]+H-L_avg-.001*H
-                
-                #calculate intermediate acceleration
-                #if the link isn't broken
-                if b_avg == 0:
-                    a1_avg = (k/(4*m))*(L_avg-y1_avg) - (ea/m)*math.atanh((d[i]-y1_avg)/(H-L_avg)) - g
-                #if the link is broken (no link force)
-                else:
-                    a1_avg = -(ea/m)*math.atanh((d[i]-y1_avg)/(H-L_avg)) - g
-                
-                #apply predictor-corrector formula to find new v
-                v_avg = v_avg + (dt/2)*(a1_avg+a_avg)
-                
-                #apply predictor-corrector formula to find new y
-                y_avg[i] = y_avg[i-1] - (dt/2)*(v1_avg+v_avg)
-                
-                #if y_avg[i] has stepped over the boundary for the allowable atanh argument, correct
-                #if y_avg[i] is too negative, increase slightly
+                #correct y value if necessary
+                #make sure the displacement doesn't pass the top of the link
                 if d[i]-y_avg[i] >= H-L_avg:
-                    y_avg[i] = d[i]-H+L_avg+.001*H
-                #if y_avg[i] is too positive, decrease slightly
-                elif d[i]-y_avg[i] <= L_avg-H:
-                    y_avg[i] = d[i]+H-L_avg-.001*H
-                
+                    y_avg[i] = d[i] - (H-L_avg)*.999999
+                #make sure the spring doesn't stretch too long
+                if d[i]-y_avg[i] <= L_avg-H:
+                    y_avg[i] = d[i] - (L_avg-H)*.999999
+            
                 #calculate force using the calculated y value
                 F_avg = ea*math.atanh((d[i]-y_avg[i])/(H-L_avg))
             
@@ -692,7 +605,13 @@ def Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn,
             y_avg[i] = L_avg
             
             #calculate the force in the link
-            F_avg = ea*math.atanh((d[i] - L_avg)/(H - L_avg))
+            #if the atanh argument is <= -1
+            if (d[i] - L_avg)/(H - L_avg) <= -1:
+                #make force as small as possible
+                F_avg = ea*math.atanh(-.9999999999999999)
+            #otherwise calculate force normally
+            else:
+                F_avg = ea*math.atanh((d[i] - L_avg)/(H - L_avg))
             
             #check to see if the link is uncollapsing
             #for dynamics, check if the spring force will overcome gravity
@@ -748,6 +667,10 @@ if multirun:
 else:
     P, P_avg, y, F_crit_avg, L = Analysis(ea_t, k_t, H, Wm, n_l, d, i_max, theta_crit, m, g, dt, t_stop, dyn, damage, bing, testing)
     
+
+#stop timing code
+print "Elapsed time is %f seconds" % (time.clock() - start_time)
+
     
 #plot
 
@@ -755,8 +678,22 @@ else:
 if multirun:
     for k in range(runs):
         plt.plot(d,P[k],'b',lw=2,alpha=0.2)
-        
-    plt.axis([0,H+.1*H,0,max(F_crit_avg)*n_l*1.5])
+    #set axes
+    if not dyn:    
+        plt.axis([0,H+.1*H,0,max(F_crit_avg)*n_l*1.5])
+    else:
+        #find min and max force values
+        #initialize
+        minP = 0
+        maxP = 0
+        for k in range(runs):
+            minPk = min(P[k])
+            maxPk = max(P[k])
+            if minPk < minP:
+                minP = minPk
+            if maxPk > maxP:
+                maxP = maxPk
+        plt.axis([0,H+.1*H,minP*1.1,maxP*1.1])
     plt.xlabel("Displacement (m)")
     plt.ylabel("Force (N)")
     plt.title("%d %d-link Force vs. Displacement Curves" % (runs,n_l))
@@ -776,9 +713,6 @@ else:
     plt.legend(["Actual Force Value","Force Value for Average Length"],fontsize = 'medium')
 
 
-
-#stop timing code
-print "Elapsed time is %f seconds" % (time.clock() - start_time)
 
 #show plot
 plt.show()
--- RK4Dyn.py
+++ RK4Dyn.py
@@ -5,53 +5,83 @@ This code implements the 4th order Runge-Kutta method for the dynamic analysis e
 import math
 
 
-def RK4(u0,u1,y0,v0,H,L,k,ea,m,g):
+def RK4(dt,u0,u1,y0,v0,H,L,k,ea,m,g):
     #inputs: u at the beginning (u0) and end (u1) of the step
     #        y and v at the beginning of the time step
     #        parameters H, L, k, ea, and m
     #outputs: y and v at the end of the time step
     
-    #find step size h
-    h = u1-u0
     
-    #find next y value
-    K_1y = v0
-    K_2y = v0 + (h/2)*K_1y
-    K_3y = v0 + (h/2)*K_2y
-    K_4y = v0 + h*K_3y
+    #find displacement step size du
+    du = u1-u0
     
-    y1 = y0 + (h/6)*(K_1y + 2*K_2y + 2*K_3y + K_4y)
+    
+    #find time step size h
+    h = dt
+    
+    
+    #find next v and y values
+    
+    #Find K_v1
+    if ((u0)-(y0))/(H-L) <= -1:
+        K_v1 = (k/(4*m))*(L-(y0)) - (ea/m)*math.atanh(-.9999999999999999) - g
+        
+    elif ((u0)-(y0))/(H-L) >= 1:
+        K_v1 = (k/(4*m))*(L-(y0)) - (ea/m)*math.atanh(.9999999999999999) - g
+        
+    else:
+        K_v1 = (k/(4*m))*(L-(y0)) - (ea/m)*math.atanh(((u0)-(y0))/(H-L)) - g
+    
+    #find K_y1
+    K_y1 = -v0
+    
+    
+    #find K_v2
+    if ((u0+(du/2))-(y0+(h/2)*K_y1))/(H-L) <= -1:
+        K_v2 = (k/(4*m))*(L-(y0+(h/2)*K_y1)) - (ea/m)*math.atanh(-.9999999999999999) - g
+        
+    elif ((u0+(du/2))-(y0+(h/2)*K_y1))/(H-L) >= 1:
+        K_v2 = (k/(4*m))*(L-(y0+(h/2)*K_y1)) - (ea/m)*math.atanh(.9999999999999999) - g
+        
+    else:
+        K_v2 = (k/(4*m))*(L-(y0+(h/2)*K_y1)) - (ea/m)*math.atanh(((u0+(du/2))-(y0+(h/2)*K_y1))/(H-L)) - g
+    
+    #find K_y2
+    K_y2 = -(v0 + (h/2)*K_v1)
+    
+    
+    #find K_v3
+    if ((u0+(du/2))-(y0+(h/2)*K_y2))/(H-L) <= -1:
+        K_v3 = (k/(4*m))*(L-(y0+(h/2)*K_y2)) - (ea/m)*math.atanh(-.9999999999999999) - g
+        
+    elif ((u0+(du/2))-(y0+(h/2)*K_y2))/(H-L) >= 1:
+        K_v3 = (k/(4*m))*(L-(y0+(h/2)*K_y2)) - (ea/m)*math.atanh(.9999999999999999) - g
+        
+    else:
+        K_v3 = (k/(4*m))*(L-(y0+(h/2)*K_y2)) - (ea/m)*math.atanh(((u0+(du/2))-(y0+(h/2)*K_y2))/(H-L)) - g
+        
+    #find K_y3
+    K_y3 = -(v0 + (h/2)*K_v2)
+    
+    
+    #find K_v4
+    if ((u1)-(y0+h*K_y3))/(H-L) <= -1:
+        K_v4 = (k/(4*m))*(L-(y0+h*K_y3)) - (ea/m)*math.atanh(-.9999999999999999) - g
+        
+    elif ((u1)-(y0+h*K_y3))/(H-L) >= 1:
+        K_v4 = (k/(4*m))*(L-(y0+h*K_y3)) - (ea/m)*math.atanh(.9999999999999999) - g
+        
+    else:
+        K_v4 = (k/(4*m))*(L-(y0+h*K_y3)) - (ea/m)*math.atanh(((u1)-(y0+h*K_y3))/(H-L)) - g
+    
+    #find K_y4
+    K_y4 = -(v0 + h*K_v3)
+        
     
     #find next v value
-    #ensure it doesn't get math errors from too large steps
-    if ((u0)-(y0))/(H-L) <= -1:
-        K_v1 = -(k/(4*m))*(L-(y0)) + (ea/m)*math.atanh(-.9999999999999999) + g
-    elif ((u0)-(y0))/(H-L) >= 1:
-        K_v1 = -(k/(4*m))*(L-(y0)) + (ea/m)*math.atanh(.9999999999999999) + g
-    else:
-        K_v1 = -(k/(4*m))*(L-(y0)) + (ea/m)*math.atanh(((u0)-(y0))/(H-L)) + g
-    
-    if ((u0+(h/2))-(y0+(h/2)*K_v1))/(H-L) <= -1:
-        K_v2 = -(k/(4*m))*(L-(y0+(h/2)*K_v1)) + (ea/m)*math.atanh(-.9999999999999999) + g
-    elif ((u0+(h/2))-(y0+(h/2)*K_v1))/(H-L) >= 1:
-        K_v2 = -(k/(4*m))*(L-(y0+(h/2)*K_v1)) + (ea/m)*math.atanh(.9999999999999999) + g
-    else:
-        K_v2 = -(k/(4*m))*(L-(y0+(h/2)*K_v1)) + (ea/m)*math.atanh(((u0+(h/2))-(y0+(h/2)*K_v1))/(H-L)) + g
-   
-    if ((u0+(h/2))-(y0+(h/2)*K_v2))/(H-L) <= -1:
-        K_v3 = -(k/(4*m))*(L-(y0+(h/2)*K_v2)) + (ea/m)*math.atanh(-.9999999999999999) + g
-    elif ((u0+(h/2))-(y0+(h/2)*K_v2))/(H-L) >= 1:
-        K_v3 = -(k/(4*m))*(L-(y0+(h/2)*K_v2)) + (ea/m)*math.atanh(.9999999999999999) + g
-    else:
-        K_v3 = -(k/(4*m))*(L-(y0+(h/2)*K_v2)) + (ea/m)*math.atanh(((u0+(h/2))-(y0+(h/2)*K_v2))/(H-L)) + g
-    
-    if ((u0+h)-(y0+h*K_v3))/(H-L) <= -1:
-        K_v4 = -(k/(4*m))*(L-(y0+h*K_v3)) + (ea/m)*math.atanh(-.9999999999999999) + g
-    elif ((u0+h)-(y0+h*K_v3))/(H-L) >= 1:
-        K_v4 = -(k/(4*m))*(L-(y0+h*K_v3)) + (ea/m)*math.atanh(.9999999999999999) + g
-    else:
-        K_v4 = -(k/(4*m))*(L-(y0+h*K_v3)) + (ea/m)*math.atanh(((u0+h)-(y0+h*K_v3))/(H-L)) + g
-    
     v1 = v0 + (h/6)*(K_v1 + 2*K_v2 + 2*K_v3 + K_v4)
+    
+    #find next y value
+    y1 = y0 + (h/6)*(K_y1 + 2*K_y2 + 2*K_y3 + K_y4)
     
     return y1,v1
